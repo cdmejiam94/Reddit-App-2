92eb57443d732c8b7448f8726e36927868511de9
cdmejiam94 <cdmejiam94@gmail.com>
2022-03-14 20:08:38 -0600

    Version 2 de app reddit sin uso de OAuth

--- main.js
+++ main.js
@@ -1,55 +1,30 @@
 const container = document.getElementById("container");
 
 const fetchList = async () => {
-    return await fetch("https://www.reddit.com/subreddits/popular.json")
-    .then((result) => {return result.json()})
-    .then((data) => {
-        return showPosts(data.data.children)
-    })
+    fetch("https://www.reddit.com/subreddits/popular.json")
+    .then((result) =>  result.json())
+    .then((data) =>  data.data.children.map((item) => getPostItem(item.data)))
 }
 
-const getPostItem = (item, element) => {
-
-    if(item.data.header_title !== null && item.data.mobile_banner_image !== null){
-        const post = document.createElement("div")
-        post.className = "col mb-4"
-        const card = document.createElement("div")
-        card.className = "card"
-        const body = document.createElement("div")
-        body.className ="card-body"
-        const title = document.createElement("h6")
-        title.className = "card-title text-uppercase"
-        title.innerHTML = item.data.header_title
-        const line = document.createElement("hr")
-        const image = document.createElement("img")
-        image.src = item.data.mobile_banner_image
-        const url = document.createElement("a")
-        url.href = `https://www.reddit.com${item.data.url}`
-        url.innerHTML = item.data.url
-
-        card.appendChild(body)
-        card.appendChild(title)
-        card.appendChild(image)
-        card.appendChild(line)
-        card.appendChild(url)
-        post.appendChild(card)
-
-        element.appendChild(post)
+const getPostItem = ({header_title, mobile_banner_image, url}) => {
+    if(!header_title || !mobile_banner_image){
+        return
     }
-}
-
-const showPosts = (list = []) => {
-    const postsContainer = document.createElement("div")
-    postsContainer.className = "row"
-    list.map((item) => {
-        getPostItem(item, postsContainer)
-    })
-    return postsContainer
+    const post = document.createElement("div")
+    post.className = "col-12 mb-4"
+    
+    const postInnertHTML = `
+    <div  class="card" > 
+        <div class="card-body">
+            <h6 class="card-title text-uppercase">${header_title} </h6>
+            <img class="mw-100" src=${mobile_banner_image} alt="">
+            </hr>
+            <a class="d-block" href=https://www.reddit.com${url}>${url} </a>
+        </div>
+    </div>
+    `
+    post.innerHTML=postInnertHTML
+    container.appendChild(post)
 }
 
 fetchList()
-.then((result) => {
-    container.appendChild(
-        result
-    )
-})
